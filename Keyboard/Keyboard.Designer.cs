54d95d13da1d442a739e52c67376ffaaa17d06db
Steve Messer <steve.g.messer@gmail.com>
2019-06-12 17:22:32 -0600

    Steve Messer initial check in to Github

---
+++ Keyboard/Keyboard.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace Keyboard
+{
+    partial class Keyboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Keyboard));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbMessage = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbKeyPressState = new System.Windows.Forms.Label();
+            this.lbCode = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lbVKey = new System.Windows.Forms.Label();
+            this.lbKey = new System.Windows.Forms.Label();
+            this.lbCaption = new System.Windows.Forms.Label();
+            this.gbDetails = new System.Windows.Forms.GroupBox();
+            this.lbSource = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lbDescription = new System.Windows.Forms.Label();
+            this.lbName = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbType = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbNumKeyboards = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbHandle = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.gbDetails.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.lbMessage);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.lbKeyPressState);
+            this.groupBox1.Controls.Add(this.lbCode);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.lbVKey);
+            this.groupBox1.Controls.Add(this.lbKey);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox1.Location = new System.Drawing.Point(20, 65);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(780, 40);
+            this.groupBox1.TabIndex = 26;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Keystroke";
+            // 
+            // lbMessage
+            // 
+            this.lbMessage.AutoSize = true;
+            this.lbMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMessage.Location = new System.Drawing.Point(486, 20);
+            this.lbMessage.Name = "lbMessage";
+            this.lbMessage.Size = new System.Drawing.Size(0, 13);
+            this.lbMessage.TabIndex = 26;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label9.Location = new System.Drawing.Point(434, 20);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 13);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Message:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(509, 20);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 25;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(255, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Key Press State : ";
+            // 
+            // lbKeyPressState
+            // 
+            this.lbKeyPressState.AutoSize = true;
+            this.lbKeyPressState.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbKeyPressState.Location = new System.Drawing.Point(346, 20);
+            this.lbKeyPressState.Name = "lbKeyPressState";
+            this.lbKeyPressState.Size = new System.Drawing.Size(0, 13);
+            this.lbKeyPressState.TabIndex = 23;
+            // 
+            // lbCode
+            // 
+            this.lbCode.AutoSize = true;
+            this.lbCode.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lbCode.Location = new System.Drawing.Point(10, 20);
+            this.lbCode.Name = "lbCode";
+            this.lbCode.Size = new System.Drawing.Size(35, 13);
+            this.lbCode.TabIndex = 8;
+            this.lbCode.Text = "Code:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label7.Location = new System.Drawing.Point(97, 20);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Virtual Key:";
+            // 
+            // lbVKey
+            // 
+            this.lbVKey.AutoSize = true;
+            this.lbVKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbVKey.Location = new System.Drawing.Point(153, 20);
+            this.lbVKey.Name = "lbVKey";
+            this.lbVKey.Size = new System.Drawing.Size(0, 13);
+            this.lbVKey.TabIndex = 21;
+            // 
+            // lbKey
+            // 
+            this.lbKey.AutoSize = true;
+            this.lbKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbKey.Location = new System.Drawing.Point(51, 20);
+            this.lbKey.Name = "lbKey";
+            this.lbKey.Size = new System.Drawing.Size(0, 13);
+            this.lbKey.TabIndex = 19;
+            // 
+            // lbCaption
+            // 
+            this.lbCaption.Location = new System.Drawing.Point(18, 19);
+            this.lbCaption.Name = "lbCaption";
+            this.lbCaption.Size = new System.Drawing.Size(652, 30);
+            this.lbCaption.TabIndex = 23;
+            this.lbCaption.Text = "Press any key on an attached keyboard to see details of the input device and the " +
+    "key(s) you pressed.";
+            // 
+            // gbDetails
+            // 
+            this.gbDetails.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbDetails.Controls.Add(this.lbSource);
+            this.gbDetails.Controls.Add(this.label8);
+            this.gbDetails.Controls.Add(this.lbDescription);
+            this.gbDetails.Controls.Add(this.lbName);
+            this.gbDetails.Controls.Add(this.label6);
+            this.gbDetails.Controls.Add(this.label1);
+            this.gbDetails.Controls.Add(this.lbType);
+            this.gbDetails.Controls.Add(this.label2);
+            this.gbDetails.Controls.Add(this.lbNumKeyboards);
+            this.gbDetails.Controls.Add(this.label4);
+            this.gbDetails.Controls.Add(this.label3);
+            this.gbDetails.Controls.Add(this.lbHandle);
+            this.gbDetails.Location = new System.Drawing.Point(17, 109);
+            this.gbDetails.Name = "gbDetails";
+            this.gbDetails.Size = new System.Drawing.Size(783, 140);
+            this.gbDetails.TabIndex = 25;
+            this.gbDetails.TabStop = false;
+            this.gbDetails.Text = "Device details";
+            // 
+            // lbSource
+            // 
+            this.lbSource.AutoSize = true;
+            this.lbSource.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSource.Location = new System.Drawing.Point(55, 108);
+            this.lbSource.Name = "lbSource";
+            this.lbSource.Size = new System.Drawing.Size(0, 13);
+            this.lbSource.TabIndex = 24;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(9, 108);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 13);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Source:";
+            // 
+            // lbDescription
+            // 
+            this.lbDescription.AutoSize = true;
+            this.lbDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDescription.Location = new System.Drawing.Point(85, 84);
+            this.lbDescription.Name = "lbDescription";
+            this.lbDescription.Size = new System.Drawing.Size(0, 13);
+            this.lbDescription.TabIndex = 22;
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbName.Location = new System.Drawing.Point(85, 60);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(0, 13);
+            this.lbName.TabIndex = 20;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 39);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "# Keyboards:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Handle:";
+            // 
+            // lbType
+            // 
+            this.lbType.AutoSize = true;
+            this.lbType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbType.Location = new System.Drawing.Point(379, 18);
+            this.lbType.Name = "lbType";
+            this.lbType.Size = new System.Drawing.Size(0, 13);
+            this.lbType.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(338, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Type:";
+            // 
+            // lbNumKeyboards
+            // 
+            this.lbNumKeyboards.AutoSize = true;
+            this.lbNumKeyboards.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNumKeyboards.Location = new System.Drawing.Point(83, 39);
+            this.lbNumKeyboards.Name = "lbNumKeyboards";
+            this.lbNumKeyboards.Size = new System.Drawing.Size(0, 13);
+            this.lbNumKeyboards.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Device Desc:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Name:";
+            // 
+            // lbHandle
+            // 
+            this.lbHandle.AutoSize = true;
+            this.lbHandle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbHandle.Location = new System.Drawing.Point(83, 18);
+            this.lbHandle.Name = "lbHandle";
+            this.lbHandle.Size = new System.Drawing.Size(0, 13);
+            this.lbHandle.TabIndex = 16;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(176, 274);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(479, 29);
+            this.textBox1.TabIndex = 27;
+            // 
+            // Keyboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 323);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbCaption);
+            this.Controls.Add(this.gbDetails);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Keyboard";
+            this.Text = "Raw Keyboard Input";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Keyboard_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.gbDetails.ResumeLayout(false);
+            this.gbDetails.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbCode;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbVKey;
+        private System.Windows.Forms.Label lbKey;
+        private System.Windows.Forms.Label lbCaption;
+        private System.Windows.Forms.GroupBox gbDetails;
+        private System.Windows.Forms.Label lbDescription;
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbType;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbNumKeyboards;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbHandle;
+        private System.Windows.Forms.Label lbSource;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbKeyPressState;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lbMessage;
+        private System.Windows.Forms.TextBox textBox1;
+
+    }
+}
+
---
+++ Keyboard/Keyboard.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using RawInput_dll;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Forms;
+
+
+namespace Keyboard
+{
+    public partial class Keyboard : Form
+    {
+        private readonly RawInput _rawinput;
+        
+        const bool CaptureOnlyInForeground = true;
+        // Todo: add checkbox to form when checked/uncheck create method to call that does the same as Keyboard ctor 
+
+        public Keyboard()
+        {
+            InitializeComponent();
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            _rawinput = new RawInput(Handle, CaptureOnlyInForeground);
+           
+            _rawinput.AddMessageFilter();   // Adding a message filter will cause keypresses to be handled
+            Win32.DeviceAudit();            // Writes a file DeviceAudit.txt to the current directory
+
+            _rawinput.KeyPressed += OnKeyPressed;   
+        }
+
+        private void OnKeyPressed(object sender, RawInputEventArg e)
+        {
+            lbHandle.Text = e.KeyPressEvent.DeviceHandle.ToString();
+            lbType.Text = e.KeyPressEvent.DeviceType;
+            lbName.Text = e.KeyPressEvent.DeviceName;
+            lbDescription.Text = e.KeyPressEvent.Name;
+            lbKey.Text = e.KeyPressEvent.VKey.ToString(CultureInfo.InvariantCulture);
+            lbNumKeyboards.Text = _rawinput.NumberOfKeyboards.ToString(CultureInfo.InvariantCulture);
+            lbVKey.Text = e.KeyPressEvent.VKeyName;
+            lbSource.Text = e.KeyPressEvent.Source;
+            lbKeyPressState.Text = e.KeyPressEvent.KeyPressState;
+            lbMessage.Text = string.Format("0x{0:X4} ({0})", e.KeyPressEvent.Message);
+           
+            //switch (e.KeyPressEvent.Message)
+            //{
+            //    case Win32.WM_KEYDOWN:
+            //        Debug.WriteLine(e.KeyPressEvent.KeyPressState);
+            //        break;
+            //     case Win32.WM_KEYUP:
+            //        Debug.WriteLine(e.KeyPressEvent.KeyPressState);
+            //        break;
+            //}
+        }
+
+        private void Keyboard_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _rawinput.KeyPressed -= OnKeyPressed;
+        }
+
+        private static void CurrentDomain_UnhandledException(Object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = e.ExceptionObject as Exception;
+
+            if (null == ex) return;
+
+            // Log this error. Logging the exception doesn't correct the problem but at least now
+            // you may have more insight as to why the exception is being thrown.
+            Debug.WriteLine("Unhandled Exception: " + ex.Message);
+            Debug.WriteLine("Unhandled Exception: " + ex);
+            MessageBox.Show(ex.Message);
+        }
+    }
+}
